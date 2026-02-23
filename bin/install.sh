f7431536a707d708d2a53686c9367846ade97f2d
Clay Rackley <crackley@Kilo.local>
2026-02-23 10:30:35 -0500

    Update install script to use crackley/Anthias fork and fix branch

--- bin/install.sh
+++ bin/install.sh
@@ -5,13 +5,13 @@
 
 set -euo pipefail
 
-BRANCH="master"
+BRANCH="fix/code-review-improvements"
 ANSIBLE_PLAYBOOK_ARGS=()
-REPOSITORY="https://github.com/Screenly/Anthias.git"
+REPOSITORY="https://github.com/crackley/Anthias.git"
 ANTHIAS_REPO_DIR="/home/${USER}/screenly"
-GITHUB_API_REPO_URL="https://api.github.com/repos/Screenly/Anthias"
-GITHUB_RELEASES_URL="https://github.com/Screenly/Anthias/releases"
-GITHUB_RAW_URL="https://raw.githubusercontent.com/Screenly/Anthias"
+GITHUB_API_REPO_URL="https://api.github.com/repos/crackley/Anthias"
+GITHUB_RELEASES_URL="https://github.com/crackley/Anthias/releases"
+GITHUB_RAW_URL="https://raw.githubusercontent.com/crackley/Anthias"
 DOCKER_TAG="latest"
 UPGRADE_SCRIPT_PATH="${ANTHIAS_REPO_DIR}/bin/upgrade_containers.sh"
 ARCHITECTURE=$(uname -m)
